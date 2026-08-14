50b8093f621743472eff13c7e4060a33b93c73d0
damianprog <damianpukszta@gmail.com>
2026-08-14 21:28:54 +0200

    MyReadonly testing homomorphic behaviour

--- 11-readonly-challenge-5.ts
+++ 11-readonly-challenge-5.ts
@@ -23,6 +23,24 @@ type MyReadonly<T> = {
   readonly [Property in keyof T]: T[Property];
 };
 
+type Suspect<T> = { readonly [P in keyof T as P]: T[P] };
+type X = Suspect<string[]>; // wciąż readonly string[]?
+
+type X2 = Suspect<string[]>;
+declare const b: X2;
+
+const lit: X2 = ["a", "b"]; // 1. czy literał tablicy pasuje?
+b[0] = "z"; // 2. czy indeks jest readonly?
+b.push("q"); // 3. czy push w ogóle istnieje?
+b.map((s) => s); // 4. czy map istnieje?
+
+declare const ro: readonly string[];
+const c1: X2 = ro; // 5. czy readonly string[] pasuje do X2?
+
+type A = MyReadonly<{ a?: string }>; // czy `?` przeżyje?
+type B = MyReadonly<string[]>; // czy dostaniesz obiekt z kluczami "0" | "1" | "push"...?
+type C = MyReadonly<{ a: 1 } | { b: 2 }>; // jeden typ czy union?
+
 /* _____________ Test Cases _____________ */
 
 interface Todo {
